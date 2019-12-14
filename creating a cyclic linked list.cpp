2cf65ebf8a476eeebb23a4137e1999fa2433d15d
Pravir Kadian <pravirkadian123@gmail.com>
2019-12-14 19:35:47 +0530

    Add files via upload

---
+++ creating a cyclic linked list.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node *next;
+
+    node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+
+void insertathead(node *&head,int data){
+    node *n=new node(data);
+    n->next=head;
+    head=n;
+}
+void print(node *head){
+    while(head!=NULL){
+        cout << head->data << "->";
+        head=head->next;
+    }
+}
+bool iscyclic(node *head){
+    node *slow=head;
+    node *fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(fast==slow){
+            return true;
+        }
+    }
+    return false;
+}
+
+void createcyclic(node *&head){
+    node *temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    node *temp1=head;
+    temp1=temp1->next;
+    temp->next=temp1;
+}
+
+int main() {
+    node *head=NULL;
+    insertathead(head,20);
+    insertathead(head,4);
+    insertathead(head,15);
+    insertathead(head,10);
+
+    if(iscyclic(head)){
+        cout << "true" << endl;
+    }
+    else{
+        cout << "false" << endl;
+    }
+    createcyclic(head);
+    if(iscyclic(head)){
+        cout << "true" << endl;
+    }
+    else{
+        cout << "false" << endl;
+    }
+   
+}
